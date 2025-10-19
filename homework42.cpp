023b2baabda65887ce86e29744d7e7c60e59c6f6
Arpi <arpikarapetyan27@gmail.com>
2025-10-19 12:12:58 +0400

    homework no.42

---
+++ homework42.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+void createMat(int **arr, int col, int row)
+{
+    for(int i = 0; i < col; i++)
+    {
+        (*arr)[i] = (int*)malloc(row * sizeof(int));
+    }
+}
+
+void initMat(int **arr, int col, int row)
+{
+    for(int i = 0; i < col; i++)
+    {
+        for(int j = 0; j < row; j++)
+        {
+            arr[i][j] = rand() % 10;
+        }
+    }
+}
+
+void printMat(int **arr, int col, int row)
+{
+    for(int i = 0; i < col; i++)
+    {
+        for(int j = 0; j < row; j++)
+        {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void deleteMat(int **arr, int col)
+{
+    for(int i = 0; i < col; i++)
+    {
+        free(arr[i]);
+    }
+    free (arr);
+}
+
+int main(int argc, char* argv[])
+{
+   int x = std::atoi(argv[1]);  
+   int y = std::atoi(argv[2]);
+
+   int col = x;
+   int row = y;
+   *arr = (int**)malloc(col * sizeof(int*));
+
+   
+    createMat(arr,col,row);
+    initMat(arr,col,row);
+    printMat(arr,col,row);
+    deleteMat(arr,col);
+   return 0;
+}

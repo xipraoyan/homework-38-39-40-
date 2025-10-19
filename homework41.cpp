c9b9f14a77e6091c45b9447f8ded31509d2fe560
Arpi <arpikarapetyan27@gmail.com>
2025-10-19 12:01:15 +0400

    homework no.41

--- homework41.cpp
+++ homework41.cpp
@@ -1,22 +1,57 @@
 #include <iostream>
+void createMat(int **arr, int col, int row)
+{
+    for(int i = 0; i < col; i++)
+    {
+        arr[i] = new int[row];
+    }
+}
+
+void initMat(int **arr, int col, int row)
+{
+    for(int i = 0; i < col; i++)
+    {
+        for(int j = 0; j < row; j++)
+        {
+            arr[i][j] = rand() % 10;
+        }
+    }
+}
+
+void printMat(int **arr, int col, int row)
+{
+    for(int i = 0; i < col; i++)
+    {
+        for(int j = 0; j < row; j++)
+        {
+            std::cout << arr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void deleteMat(int **arr, int col)
+{
+    for(int i = 0; i < col; i++)
+    {
+        delete [] arr[i];
+    }
+    delete [] arr;
+}
 
 int main(int argc, char* argv[])
 {
-   int x;
-   std::cin >> x;
+   int x = std::atoi(argv[1]);  
+   int y = std::atoi(argv[2]);
 
-   int size = x;
-   int* arr = new int[size];
+   int col = x;
+   int row = y;
+   int** arr = new int*[col];
 
-   for(int i = 0; i < size; i++)
-   {
-    arr[i] = rand() % 10;
-   }
-
-   for(int i = 0; i < size; i++)
-   {
-      std::cout << arr[i] << " " << std::endl;
-   }
-   delete [] arr;
+   
+    createMat(arr,col,row);
+    initMat(arr,col,row);
+    printMat(arr,col,row);
+    deleteMat(arr,col);
    return 0;
 }

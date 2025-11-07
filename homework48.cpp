821a79b53986afa826a0feb5f048a9d47aead21f
Arpi <arpikarapetyan27@gmail.com>
2025-11-07 11:22:46 +0400

    homework no.48

--- homework48.cpp
+++ homework48.cpp
@@ -16,25 +16,57 @@ class Matrix
        for(int i = 0; i < m_col; i++)
         {
            m_arr[i] = new int[m_row];
-        }
+           for(int j = 0; j < m_row; j++)
+           {
+               m_arr[i][j] = rand() % 10; 
+           }
+       }
+        
     }
 
-    Matrix(const Matrix& other)
+    Matrix(const Matrix& other) //copy ctor
     {
         m_row = other.m_row;
         m_col = other.m_col;
-        
-        for(int i = 0; i < m_col; i++)
+
+        m_arr = new int* [m_col];
+       for(int i = 0; i < m_col; i++)
         {
-          for(int j = 0; j < m_row; j++)
-          {
-            std::cout << m_arr[i][j] << " ";
-          }
-          std::cout << std::endl;
+           m_arr[i] = new int[m_row];
+           for(int j = 0; j < m_row; j++)
+           {
+             m_arr[i][j] = other.m_arr[i][j];
+             std::cout << m_arr[i][j] << " ";
+           }
+           std::cout << std::endl;
         }
     }
 
-    ~Matrix()
+    Matrix& operator = (const Matrix& other) //operator =
+    {
+        if(this != &other)
+        {
+
+            this -> m_row = other.m_row;
+            this -> m_col = other.m_col;
+
+            this -> m_arr = new int* [m_col];
+            for(int i = 0; i < m_col; i++)
+            {
+               m_arr[i] = new int[m_row];
+               for(int j = 0; j < m_row; j++)
+               {
+                m_arr[i][j] = other.m_arr[i][j];
+                std::cout << m_arr[i][j] << " ";
+               }
+              std::cout << std::endl;
+            }  
+        }
+
+        return *this;
+    }
+
+    ~Matrix()  //destructor
     {
         for (int i = 0; i < m_col; ++i) 
         {
@@ -42,13 +74,48 @@ class Matrix
         }
         delete [] m_arr;
     }
+
+
+    /*
+    void set_Matrix(int a, int b)
+    {
+        m_col = a;
+        m_row = b;
+    }
+
+    int get_col() const
+    {
+      return m_col;
+    }
+
+    int get_row() const
+    {
+      return m_row;
+    }
+    void print()
+    {
+        std::cout << "Columns:" << " " << get_col() << std::endl;
+        std::cout << "Rows:" << " " << get_row() << std::endl;
+    }
+    */
 };
 
 int main(int argc, char* argv[])
 {
+
+    std::cout << std::endl;
     Matrix a(3, 3);
+    //std::cout << std::endl;
+    
 
     Matrix b(a);
+    std::cout << std::endl;
+
+    Matrix c(a);
+    std::cout << std::endl;
+    c = a;
+
+    
     
     return 0;
 

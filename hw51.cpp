537c0437a1f43f34aaa5f0f2fabdd835729c0787
Arpi <arpikarapetyan27@gmail.com>
2025-11-08 15:06:32 +0400

    homework no.51

--- hw51.cpp
+++ hw51.cpp
@@ -2,71 +2,109 @@
 
 class Matrix
 {
-    private:
-    int m_col;
-    int m_row;
-    int** m_arr;
+ private:
+  int m_col;
+  int m_row;
+  int** m_arr;
 
-    public:
-    Matrix(int a, int b)
+ public:
+  Matrix(int a, int b)
+  {
+    m_col = a;
+    m_row = b;
+         
+    m_arr = new int* [m_col];
+    for(int i = 0; i < m_col; i++)
     {
-      m_col = a;
-      m_row = b;
-        
-      m_arr = new int* [m_col];
+      m_arr[i] = new int[m_row];
+      for(int j = 0; j < m_row; j++)
+      {
+        m_arr[i][j] = rand() % 10;
+      }
+    } 
+  }
+
+  Matrix(const Matrix& other)
+  {
+    m_col = other.m_col;
+    m_row = other.m_row;
+
+    m_arr = new int* [m_col];
+    for(int i = 0; i < m_col; i++)
+    {
+      m_arr[i] = new int[m_row];
+      for(int j = 0; j < m_row; j++)
+      {
+        m_arr[i][j] = other.m_arr[i][j];
+      }
+      // std::cout << std::endl;
+    }
+  }
+
+  Matrix& operator = (const Matrix& other)
+  {
+    if(this != &other)
+    {
+      this -> m_row = other.m_row;
+      this -> m_col = other.m_col;
+
+      this -> m_arr = new int* [m_col];
       for(int i = 0; i < m_col; i++)
       {
         m_arr[i] = new int[m_row];
         for(int j = 0; j < m_row; j++)
         {
-          m_arr[i][j] = rand() % 10;
-        }
-      } 
-    }
-
-    Matrix operator * (int n)
-    {
-      for(int i = 0; i < m_col; i++)
-      {
-        for(int j = 0; j < m_row; j++)
-        {
-          this -> m_arr[i][j] = this -> m_arr[i][j] * n;
-        }
-      }
-      return *this;
-    }
-
-    void print()
-    {
-      for(int i = 0; i < m_col; i++)
-      {
-        for(int j = 0; j < m_row; j++)
-        {
-          std::cout << m_arr[i][j] << " ";
-        }
-        std::cout << std::endl;
+          m_arr[i][j] = other.m_arr[i][j];
        }
+      }  
     }
+    return *this;
+  }
 
-    ~Matrix()
+  Matrix operator * (int n)
+  {
+    for(int i = 0; i < m_col; i++)
     {
-      for(int i = 0; i < m_col; i++)
+      for(int j = 0; j < m_row; j++)
       {
-        delete [] m_arr[i];
+        this -> m_arr[i][j] = this -> m_arr[i][j] * n;
       }
-      delete [] m_arr;
     }
+    return *this;
+  }
+
+  void print()
+  {
+    for(int i = 0; i < m_col; i++)
+    {
+      for(int j = 0; j < m_row; j++)
+      {
+        std::cout << m_arr[i][j] << " ";
+      }
+      std::cout << std::endl;
+    }
+  }
+   
+  ~Matrix()
+  {
+    for(int i = 0; i < m_col; i++)
+    {
+      delete [] m_arr[i];
+    }
+    delete [] m_arr;
+  }
 };
 
-   int main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
-    Matrix a(3, 3);
-    a.print();
-    std::cout << std::endl;
+  Matrix a(3, 3);
+  a.print();
+  std::cout << std::endl;
 
-    Matrix b = a * 5;
-    b.print();
+  Matrix b(a);
 
+  b = a * 5;
+  b.print();
 
-    return 0;
+  return 0;
 }
